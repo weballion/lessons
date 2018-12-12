66cc1694889e7f3107ce65455e062c7c6a122e5c
Aleksey Semenovich <aleksey.semenovich@gmail.com>
2018-12-12 13:31:50 +0200

    no message

--- header.php
+++ header.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en" class="uk-background-default">
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="css/custom.css" />
-	<link rel="stylesheet" href="css/uikit.css" />
-	<script src="js/uikit.js"></script>
-	<script src="js/uikit-icons.min.js"></script>
-	<title><?php echo $title; ?></title>
-</head>
-<body class="uk-container uk-background-muted" uk-height-viewport="expand: true">
-	<h1 class="uk-text-center uk-margin-large uk-margin-large-top"><?php echo $title; ?></h1>
-	<div class="uk-section">
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/uikit.css" />
+<link rel="stylesheet" href="css/custom.css" />
+<link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon.png">
+<link rel="shortcut icon" href="images/fav.png">
+<script src="js/uikit.js"></script>
+<script src="js/uikit-icons.min.js"></script>
+<title><?php echo $title; ?></title>
--- index.php
+++ index.php
@@ -22,14 +22,7 @@
 <!DOCTYPE html>
 <html lang="en" class="uk-background-muted">
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/uikit.css" />
-    <link rel="stylesheet" href="css/custom.css" />
-    <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon.png">
-    <link rel="shortcut icon" href="images/fav.png">
-    <script src="js/uikit.js"></script>
-    <script src="js/uikit-icons.min.js"></script>
-	<title><?php echo $title; ?></title>
+    <?php include 'header.php'; ?>
 </head>
 <body>
     <div class="uk-section uk-position-relative uk-width-1-1">
